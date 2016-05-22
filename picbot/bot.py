762d2ec3f275c706e66d77f464a9375b18821d28
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-05-22 13:06:54 +0200

    Ajout de commentaires pour aider à situer les méthodes et les objectifs. Changé de place les variables team et channel pour éviter certaines rétitions.

--- picbot/bot.py
+++ picbot/bot.py
@@ -6,31 +6,48 @@ import aiohttp
 from api import api_call
 from config import DEBUG, TOKEN
 
+#C'est dans consumer que l'essentiel du traitement des messages se fait.as
+
+#Pour le moment, le bot :
+#   -Ne répond que lorsqu'on s'addresse à lui, sous forme de texte.
+#   -Répond aux commandes pic, picture, insult et help.
+#   -Dirige les utilisateurs vers la commande help si aucune bonne commande entrée.
+
+#   -TO DO: Faire en sorte que le bot upload une image du répertoire "Images" en réponse aux commandes pic et picture.
+
 async def consumer(message, ws):
     """Consume the message by printing them."""
+    channel = "G1AN77A0L"
+    team = "T0LPWE4R5"
     print(message)
-    if message.get('type') == 'message' and message.get('channel') == 'G1AN77A0L':
-        user = await api_call('users.info',
+
+    if message.get('type') == 'message' and message.get('channel') == channel:
+        user = await api_call('users.info',     #récupère le nom de l'utilisateur ayant envoyé le message
                               {'user': message.get('user')})
 
-        channel = "G1AN77A0L"
-        team = "T0LPWE4R5"
-
+        #sépare le message en deux, avec d'un côté le destinataire du message, de l'autre le corps du message.
         message_split = message.get('text').split(':', 1)
         recipient = message_split[0]
         core_text = message_split[1]
 
-        if(recipient == '<@U145RGCDS>'):
-            answer = bot_answers(core_text.strip())
+        if recipient == '<@U145RGCDS>': #Si on s'adresse au bot
+            answer = bot_answers(core_text.strip()) #La methode "strip()" enlève les espaces superflus ("   picture  " sera considéré comme "picture")
+
+            #la méthode ci-dessous envoie sur le web socket un message sous forme de string.
             ws.send_str(json.dumps({"type": "message",
                                 "channel": channel,
                                 "text": "<@{0}> {1}".format(user["user"]["name"], answer),
                                 "team": team}))
+
+            #Ce code ci dessous ne marche pas.
+            #Cependant, j'ai cru comprendre que pour envoyé une image il faut utiliser "api_call" avec une méthode "file.upload"
+            #Il faut checker à cette adresse, ça peut aider : https://medium.com/@greut/a-slack-bot-with-pythons-3-5-asyncio-ad766d8b5d8f#.f3uy2tyne
             #uploaded = await api_call('files.upload', {'file': 'Images/meme1.jpg',
             #                                           "channel": channel,
             #                                           "team": team})
 
 
+#Retourne un message en fonction du message entré, avec une valeur par défaut si x n'est pas pris en charge.
 def bot_answers(x):
     return {
         'pic': 'The picture module is still in development.',
@@ -43,6 +60,7 @@ def bot_answers(x):
                               "Command 'insult' : insults you like a Sir.\n"
                               "Command 'help' : you know what this does.\n",
     }.get(x, 'Command not found. Type "help" for a list of commands available.')
+
 
 async def bot(token):
     """Create a bot that joins Slack."""
