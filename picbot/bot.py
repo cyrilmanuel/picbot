1463bd337654ab3c680220ba5e4c5e30f6451873
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-05-27 08:39:11 +0200

    Bot fonctionnel ! Largement commenté pour permettre une bonne compréhension du code et une réutilisation de celui-ci. Le bot met un lien sur une image random de xkcd, grâce au module du même nom. Le code reste améliorable cependant, mais au moins, ça marche et c'est bien.

--- picbot/bot.py
+++ picbot/bot.py
@@ -3,21 +3,25 @@ import asyncio
 import json
 import aiohttp
 import random
+import xkcd
 
 from api import api_call
 from config import DEBUG, TOKEN
 
 
-# Pour le moment, le bot :
+# Le bot :
 #   -Ne répond que lorsqu'on s'addresse à lui, sous forme de texte.
-#   -Répond aux commandes pic, picture et help.
+#   -Répond aux commandes pic, picture, joke et help.
 #   -Dirige les utilisateurs vers la commande help si aucune bonne commande entrée.
-
-#   -TO DO: Faire en sorte que le bot upload une image du répertoire "Images" en réponse aux commandes pic et picture.
 
 # Liens utiles :
 #   -Méthodes disponibles pour api_call : https://api.slack.com/methods
 #   -Guide du prof : https://medium.com/@greut/a-slack-bot-with-pythons-3-5-asyncio-ad766d8b5d8f#.7okn8gngi
+
+# Notes :
+#   -Le bot n'uplload pas d'images, il va les chercher déjà existantes sur Internet.
+#    La méthode file.upload permet de mettre une image en ligne sur les serveurs Slack.
+#    Pour l'afficher, il faut ensuite aller chercher son URL, présent dans le texte retourné par la méthode file.upload.
 
 class PictBot:
     def __init__(self, token=TOKEN):
@@ -38,38 +42,35 @@ class PictBot:
             "programmer (noun) An organism capable of converting caffeine into code."
         ]
 
-    async def sendText(self, message, channel_id, user_name, team_id, *args):
+    async def sendText(self, message, channel_id, user_name, team_id):
+        """Sends a text message to the channel"""
         return await api_call('chat.postMessage', {"type": "message",
                                                    "channel": channel_id,
                                                    "text": "<@{0}> {1}".format(user_name["user"]["name"], message),
                                                    "team": team_id})
 
-    async def joke(self, channel_id, user_name, team_id, *args):
-        return await self.sendText(random.choice(self.jokes), channel_id, user_name, team_id, *args)
+    async def joke(self, channel_id, user_name, team_id):
+        """Selects randomly a joke from the list"""
+        return await self.sendText(random.choice(self.jokes), channel_id, user_name, team_id)
 
-    async def picture(self, channel_id, user_name, team_id, file_path, name_of_file):
-        with open(file_path, 'rb') as f:
-            file_uploaded = await api_call('files.upload',
-                                           {"channel": channel_id,
-                                            "team": team_id,
-                                            "text": "<@{0}>".format(user_name["user"]["name"]),
-                                            "filename": name_of_file},
-                                           file=f
-                                           )
-            return file_uploaded
+    async def picture(self, channel_id, user_name, team_id):
+        """Sends a picture to the channel"""
+        return await self.sendText(xkcd.getRandomComic().getImageLink(), channel_id, user_name, team_id)
 
-    async def help(self, channel_id, user_name, team_id, *args):
+    async def help(self, channel_id, user_name, team_id):
+        """Displays the help message to the channel"""
         helpMessage = "Welcome to our Picture bot ! \n" \
                       "This bot is here to send you some funny pictures from some funny websites. \n" \
                       "Here are the commands : \n" \
                       " - pic : uploads a picture. \n" \
-                      " - picture : one and the same =P \n" \
-                      " - joke : gets you a random joke for you, programmer" \
-                      " - help : I think you already know this one, don't you. \n" \
+                      " - picture : one and the same. =P \n" \
+                      " - joke : gets you a random joke for you, programmer. \n" \
+                      " - help : You already know this one, don't you. \n" \
                       "Have fun !"
         return await self.sendText(helpMessage, channel_id, user_name, team_id)
 
-    async def error(self, channel_id, user_name, team_id, *args):
+    async def error(self, channel_id, user_name, team_id):
+        """displays the error message to the channel, in case of bad input"""
         error = "Command not found. Type 'help' for a list of valid commands."
         return await self.sendText(error, channel_id, user_name, team_id)
 
@@ -106,6 +107,8 @@ class PictBot:
             bot_name = await api_call('users.info',  # gets the name of self
                                       {'user': bot_id})
 
+            # message related entries
+            message_text = message.get('text')
             # Prints input message. May contain useful information for coding, debugging, etc.
             print("message : {0}".format(message))
 
@@ -116,19 +119,18 @@ class PictBot:
             # _____________________________________________
 
             # Splits message in half, with recipient on the left side, and the core text on the other.
-            message_split = message.get('text').split(':', 1)
-            recipient = message_split[0].strip()
+            if(isinstance(message_text, str)):
+                message_split = message_text.split(':', 1) # Generate an exception if type is different than text.
+                recipient = message_split[0].strip()
 
-            if len(message_split) > 0 and recipient == '<@{0}>'.format(bot_id):  # If message is adressed to our bot
-                core_text = message_split[1].strip()
-                print(await self.api.setdefault(core_text, self.error)(channel_id,
-                                                                       user_name,
-                                                                       team_id,
-                                                                       'Images/meme1.jpg',
-                                                                       'pic.png'))
+                if len(message_split) > 0 and recipient == '<@{0}>'.format(bot_id):  # If message is adressed to our bot
+                    core_text = message_split[1].strip()
+                    print(await self.api.setdefault(core_text, self.error)(channel_id,
+                                                                           user_name,
+                                                                           team_id))
 
     async def connect(self):
-        """Create a bot that joins Slack."""
+        """Connects the bot to Slack"""
 
         self.rtm = await api_call('rtm.start')
         assert self.rtm['ok'], self.rtm['error']
